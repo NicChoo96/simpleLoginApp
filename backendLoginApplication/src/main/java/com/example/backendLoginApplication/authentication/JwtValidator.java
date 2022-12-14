ca31c05c41fa7557eef923eb764214e90f2383d7
Nicholas Choo <nic_choo_96@hotmail.com>
2022-12-14 13:25:14 +0800

    fix: Updated gradle to properly read version 19 JDK

---
+++ backendLoginApplication/src/main/java/com/example/backendLoginApplication/authentication/JwtValidator.java
@@ -0,0 +1,10 @@
+package com.example.backendLoginApplication.authentication;
+
+public class JwtValidator {
+
+    private final String secretKey = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY3MDk4NDMyNSwiaWF0IjoxNjcwOTg0MzI1fQ.j88QzIv7jFbxX1JEcoKba-OHNSoomUB6S__rhh0Jwq4";
+
+    public JwtValidator(){
+
+    }
+}
